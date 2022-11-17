13b444d493cbd428bdac74a23a1e98739ba3a602
RGLSA <115712997+RGLSA@users.noreply.github.com>
2022-11-17 02:21:02 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Student 1.4!"
+    return "Hello Student 1.3!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
